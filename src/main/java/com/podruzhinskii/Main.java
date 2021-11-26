bdc7cbbe94ed7ebeaa36ef537a9701cb28676bd2
evgenii-podruzhinskii <evg.pod.dm@gmail.com>
2021-11-26 13:10:09 +0300

    Add init SQL-scripts

--- src/main/java/com/podruzhinskii/Main.java
+++ src/main/java/com/podruzhinskii/Main.java
@@ -2,6 +2,7 @@ package main.java.com.podruzhinskii;
 
 public class Main {
     public static void main(String[] args) {
+        //TODO: нужно вынести удаление и инициализацию бд в отдельную хуйню, чтоб все было по красоте
         UserInteraction interaction = new UserInteraction();
         interaction.interact();
     }
