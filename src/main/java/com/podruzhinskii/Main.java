998f21b83bb77a83c60f4b271ba38be90eb898ad
evgenii-podruzhinskii <evg.pod.dm@gmail.com>
2021-11-26 13:11:32 +0300

    Remove cursed comment

--- src/main/java/com/podruzhinskii/Main.java
+++ src/main/java/com/podruzhinskii/Main.java
@@ -2,7 +2,6 @@ package main.java.com.podruzhinskii;
 
 public class Main {
     public static void main(String[] args) {
-        //TODO: нужно вынести удаление и инициализацию бд в отдельную хуйню, чтоб все было по красоте
         UserInteraction interaction = new UserInteraction();
         interaction.interact();
     }
